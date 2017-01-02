0b5562877b363c09799f0ebd749f1bf2a491e915
doumdi <doumdi@gmail.com>
2017-01-02 16:35:51 -0500

    First test with protobuf working

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,30 @@
+#include <QCoreApplication>
+#include <qDebug>
+#include <QAbstractOAuth2>
+
+#include "main.h"
+
+//Generated protobuf includes
+#include "header.pb.h"
+
+
 int main(int argc, char* argv[])
 {
-    return 0;
+    qDebug("Starting...");
+    QCoreApplication app(argc,argv);
+
+    Person person;
+
+    person.set_name("Test");
+    person.set_id(0);
+    person.set_email("test@gmail.com");
+
+    QString test = QString::fromStdString(person.SerializeAsString());
+
+    Person person2;
+    person2.ParseFromString(test.toStdString());
+
+    qDebug() << test << QString::fromStdString(person2.SerializeAsString());
+
+    app.exec();
 }
