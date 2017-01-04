34dede6fcc026f14ec2697f2a8e2e48ab5f37007
doumdi <doumdi@gmail.com>
2017-01-04 13:36:19 -0500

    testing protobuf

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,17 @@
 //Generated protobuf includes
 #include "header.pb.h"
 
+using namespace pubsub;
 
 int main(int argc, char* argv[])
 {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
     qDebug("Starting...");
     QCoreApplication app(argc,argv);
 
-    Person person;
-
-    person.set_name("Test");
-    person.set_id(0);
-    person.set_email("test@gmail.com");
-
-    QString test = QString::fromStdString(person.SerializeAsString());
-
-    Person person2;
-    person2.ParseFromString(test.toStdString());
-
-    qDebug() << test << QString::fromStdString(person2.SerializeAsString());
 
     app.exec();
+
+    // Optional:  Delete all global objects allocated by libprotobuf.
+    google::protobuf::ShutdownProtobufLibrary();
 }
